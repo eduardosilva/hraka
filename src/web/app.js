7bb3a97c0595d65d56f8ee39c7f3533faa031dba
Eduardo Silva <eduardo.lour.silva@gmail.com>
2017-11-16 18:31:58 -0200

    improve folders

--- src/web/app.js
+++ src/web/app.js
@@ -106,7 +106,7 @@ MetronicApp.controller('HeaderController', ['$scope', function($scope) {
 /* Setup Layout Part - Sidebar */
 MetronicApp.controller('PageHeadController', ['$scope', function($scope) {
     $scope.$on('$includeContentLoaded', function() {        
-        Demo.init(); // init theme panel
+        //Demo.init(); // init theme panel
     });
 }]);
 
